3bac442a380bf521be4baff714888582803300fe
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-05-24 22:22:53 +0100

    Setup stack at startup

--- monitor_src/startup.s
+++ monitor_src/startup.s
@@ -1,3 +1,4 @@
+/* vim: set tabstop=8:shiftwidth=8:expandtab */
 	.syntax	unified
 	.cpu	cortex-m4	
 	.thumb
@@ -5,6 +6,12 @@
 	.global	startup_entry
 	.type 	startup_entry, %function
 startup_entry:
+	// Setup stack // TODO: No need to do if started from flash due to vector table
+	ldr	r0,	=initial_initial_top
+	mov	sp,	r0
+
+	// TODO: Set VTOR to vector table in ram
+
 	nop
 	nop
 	nop
