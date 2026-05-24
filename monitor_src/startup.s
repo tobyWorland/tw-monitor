f91aa1280d97520a2949f4d53dfd6d07df6c7f11
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-05-24 23:56:25 +0100

    Fix vim modeline and :retab

--- monitor_src/startup.s
+++ monitor_src/startup.s
@@ -1,45 +1,45 @@
-/* vim: set tabstop=8:shiftwidth=8:expandtab */
-	.syntax	unified
-	.cpu	cortex-m4
-	.thumb
+/* vim: set tabstop=8 shiftwidth=8 expandtab : */
+        .syntax unified
+        .cpu    cortex-m4
+        .thumb
 
-	.global	startup_entry
-	.type	startup_entry, %function
+        .global startup_entry
+        .type   startup_entry, %function
 startup_entry:
-	// Setup stack // TODO: No need to do if started from flash due to vector table
-	ldr	r0,	=stack_initial_top
-	mov	sp,	r0
+        // Setup stack // TODO: No need to do if started from flash due to vector table
+        ldr     r0,     =stack_initial_top
+        mov     sp,     r0
 
-	// TODO: Set VTOR to vector table in ram
+        // TODO: Set VTOR to vector table in ram
 
-	bl	hardware_init
+        bl      hardware_init
 
-	ldr	r0,	=str_monitor
-	bl	putstring
+        ldr     r0,     =str_monitor
+        bl      putstring
 
 1:
-	bl	getchar
+        bl      getchar
 
-	// Check char is in printable range
-	cmp	r0,	' '
-	ble	1b
-	cmp	r0,	'~'
-	bgt	1b
+        // Check char is in printable range
+        cmp     r0,     ' '
+        ble     1b
+        cmp     r0,     '~'
+        bgt     1b
 
-	mov	r4,	r0
-	ldr	r0,	=str_char
-	bl	putstring
-	mov	r0,	r4
-	bl	putchar
-	bl	putnewline
-	b	1b
+        mov     r4,     r0
+        ldr     r0,     =str_char
+        bl      putstring
+        mov     r0,     r4
+        bl      putchar
+        bl      putnewline
+        b       1b
 
-	.global	hang
-	.type	hang, %function
+        .global hang
+        .type   hang, %function
 hang:
-	bkpt	90
-	b	hang
+        bkpt    90
+        b       hang
 
-	.data
-str_monitor:	.asciz "Monitor\r\n"
-str_char:	.asciz "CHAR: "
+        .data
+str_monitor:    .asciz "Monitor\r\n"
+str_char:       .asciz "CHAR: "
