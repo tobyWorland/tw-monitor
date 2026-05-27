60ae7b6b9615397bdab243a669d9054cadb5ee88
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-05-27 18:43:56 +0100

    Test char_to_upper and char_isprint

--- tests/char_test.cpp
+++ tests/char_test.cpp
@@ -4,6 +4,19 @@ extern "C" {
 #include "../monitor_src/char.h"
 }
 
+TEST(char_type, isprint) {
+    for (int i = 0; i <= 0xFF; i++) {
+        char c = static_cast<char>(i);
+        EXPECT_EQ(char_isprint(c), !!std::isprint(c));
+    }
+}
+
+TEST(case_conversion, uppercase) {
+    for (char c = 'a'; c <= 'z'; c++) {
+        EXPECT_EQ(char_to_upper(c), std::toupper(c));
+    }
+}
+
 TEST(digit_conversion, digits) {
     const char *digit_chars{"0123456789ABCDEF"};
     for (int i = 0; i <= 0x0F; i++) {
