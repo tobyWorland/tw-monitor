5b71e0844931d5ebec6e55d059dfc2d9b5bb5f30
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-05-28 23:20:42 +0100

    Split enter code up in the hope it'll make it easier to work on

--- monitor_src/monitor_cmds/monitor_enter.c
+++ monitor_src/monitor_cmds/monitor_enter.c
@@ -10,41 +10,140 @@
 #include <stdint.h>
 
 #ifndef HOST
-static void enter_load_address(const void *addr, void *dest, enum enter_type ent_type) {
-    switch (ent_type) {
+
+struct enter_state {
+    void *addr;
+    uint32_t current;
+    unsigned digit_idx;
+    unsigned col;
+
+    const enum enter_type ent_type;
+    const unsigned digit_width;
+    const bool extra_space_in_number;
+};
+
+static void load_current(struct enter_state *state) {
+    switch (state->ent_type) {
     case ET_BYTE:
-        *(uint8_t*)dest = *(uint8_t*)addr;
+        *(uint8_t*)&state->current = *(uint8_t*)state->addr;
         break;
     case ET_HWORD:
-        *(uint16_t*)dest = *(uint16_t*)addr;
+        *(uint16_t*)&state->current = *(uint16_t*)state->addr;
         break;
     case ET_WORD:
-        *(uint32_t*)dest = *(uint32_t*)addr;
+        *(uint32_t*)&state->current = *(uint32_t*)state->addr;
         break;
     default:
         ASSERT_NOT_REACHED();
     }
 };
 
-static void enter_store_address(void *addr, const void *src, enum enter_type ent_type) {
-    switch (ent_type) {
+static void store_current(struct enter_state *state) {
+    switch (state->ent_type) {
     case ET_BYTE:
-        *(uint8_t*)addr = *(uint8_t*)src;
+        *(uint8_t*)state->addr = *(uint8_t*)&state->current;
         break;
     case ET_HWORD:
-        *(uint16_t*)addr = *(uint16_t*)src;
+        *(uint16_t*)state->addr = *(uint16_t*)&state->current;
         break;
     case ET_WORD:
-        *(uint32_t*)addr = *(uint32_t*)src;
+        *(uint32_t*)state->addr = *(uint32_t*)&state->current;
         break;
     default:
         ASSERT_NOT_REACHED();
     }
 };
 
-static unsigned enter_get_digits_from_type(enum enter_type ent_type) {
+static inline unsigned enter_get_digits_from_type(enum enter_type ent_type) {
     unsigned width_in_bytes = (unsigned)ent_type;
     return width_in_bytes * 2;
+}
+
+static inline bool handle_input(struct enter_state *state) {
+    char c = getchar();
+    uint8_t digit;
+
+    if (c == '\r') {
+        return false; // Quit
+    } else if (c == '\b') {
+        if (state->digit_idx == 0) {
+            // Go back a byte
+            if (state->col > 0) {
+                state->col--;
+                state->addr -= (unsigned)state->ent_type;
+
+                vt_blank_last_n_chars(state->digit_width + 1 + !!state->extra_space_in_number);
+
+                load_current(state);
+                state->digit_idx = 0;
+            } else {
+                // TODO: handle going back a row
+            }
+        } else {
+            // Reload byte from memory
+            load_current(state);
+            state->digit_idx = 0;
+        }
+    } else if (c == ' ') {
+        // Leave entry as is and enter the next one by setting the idx as if we
+        // had entered enough digits to enter the next entry
+        state->digit_idx = state->digit_width;
+    } else if ((digit = char_to_digit(c)) != 0xFF) {
+        // Add new digit to current
+        state->current <<= 4;
+        state->current |= digit;
+        state->digit_idx++;
+    }
+
+    return true;
+}
+
+static void _monitor_enter(struct enter_state *state) {
+    load_current(state);
+
+    puthexword((uint32_t)state->addr);
+
+    while (1) {
+        // Print current entry
+        putchar(' ');
+        puthexnumber(state->digit_width, state->current);
+
+        if (state->digit_idx >= state->digit_width) {
+            // Store current to memory
+            store_current(state);
+
+            // Move to next entry
+            state->addr += (unsigned)state->ent_type;
+            load_current(state);
+            state->digit_idx = 0;
+
+            // If hit max column, we want to move to the next row
+            // 16 bytes
+            // 8  half words
+            // 4  words
+            if (++state->col == 32/state->digit_width) {
+                putnewline();
+                puthexword((uint32_t)state->addr);
+                state->col = 0;
+            }
+
+            // Print next entry
+            putchar(' ');
+            puthexnumber(state->digit_width, state->current);
+        }
+
+        if (!handle_input(state)) {
+            break;
+        }
+
+        // Erase the current entry from the screen so we can reprint it with the new digit
+        // +1 for the space
+        vt_blank_last_n_chars(state->digit_width + 1 + !!state->extra_space_in_number);
+    }
+
+    // Store our changes to the current entry before we exit
+    store_current(state);
+    putnewline();
 }
 
 enum enter_type enter_ent_type_submenu(void) {
@@ -70,78 +169,17 @@ enum enter_type enter_ent_type_submenu(void) {
 }
 
 void monitor_enter(void *addr, enum enter_type ent_type) {
-    uint32_t current;
-    unsigned digit_idx = 0;
-    unsigned col = 0;
-
     const unsigned digit_width = enter_get_digits_from_type(ent_type);
-    const bool extra_space_in_number = digit_width > 4;
+    struct enter_state state = (struct enter_state){
+        .addr = addr,
+        .ent_type = ent_type,
+        .digit_width = digit_width,
+        .extra_space_in_number = digit_width > 4,
+    };
 
     assert(ent_type != ET_INSTRUCTION); // TODO: Not supported yet
 
-    enter_load_address(addr, &current, ent_type);
-
-    puthexword((uint32_t)addr);
-    putchar(' ');
-
-    while (1) {
-        char c;
-        uint8_t digit;
-
-        puthexnumber(digit_width, current);
-
-        if (digit_idx >= digit_width) {
-            enter_store_address(addr, &current, ent_type);
-            addr += (unsigned)ent_type;
-            enter_load_address(addr, &current, ent_type);
-            digit_idx = 0;
-
-            if (++col == 16) {
-                putnewline();
-                puthexword((uint32_t)addr);
-                col = 0;
-            }
-
-            putchar(' ');
-            puthexnumber(digit_width, current);
-        }
-
-        c = getchar();
-
-        if (c == '\r') {
-            break;
-        } else if (c == '\b') {
-            if (digit_idx == 0) {
-                // Go back a byte
-                if (col > 0) {
-                    col--;
-                    addr -= (unsigned)ent_type;
-
-                    // + 1 for the space between entries
-                    vt_blank_last_n_chars(digit_width + 1 + !!extra_space_in_number);
-
-                    enter_load_address(addr, &current, ent_type);
-                    digit_idx = 0;
-                } else {
-                    // TODO: handle going back a row
-                }
-            } else {
-                // Reload byte from memory
-                enter_load_address(addr, &current, ent_type);
-                digit_idx = 0;
-            }
-        } else if (c == ' ') { // Leave entry as is and enter the next one
-            digit_idx = digit_width; // Trigger the next entry logic on the next iteration of the loop
-        } else if ((digit = char_to_digit(c)) != 0xFF) {
-            current <<= 4;
-            current |= digit;
-            digit_idx++;
-        }
-
-        vt_blank_last_n_chars(digit_width + !!extra_space_in_number);
-    }
-
-    enter_store_address(addr, &current, ent_type);
-    putnewline();
+    _monitor_enter(&state);
 }
+
 #endif
