bdfacda2cfd676d7e5bb196c406d90b2a7d7e147
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-01 18:11:42 +0100

    Add OFFSET_OF

--- monitor_src/util.h
+++ monitor_src/util.h
@@ -1,8 +1,9 @@
 #ifndef UTIL_H_INCLUDED
 #define UTIL_H_INCLUDED
 
-#define BIT(N)       (1 << N)
-#define ARR_LEN(ARR) (sizeof(ARR) / sizeof(ARR[0]))
-#define MIN(A, B)    ((A) < (B) ? (A) : (B))
+#define BIT(N)                   (1 << N)
+#define ARR_LEN(ARR)             (sizeof(ARR) / sizeof(ARR[0]))
+#define MIN(A, B)                ((A) < (B) ? (A) : (B))
+#define OFFSET_OF(STRUCT, FIELD) ((unsigned)&((STRUCT*)NULL)->FIELD)
 
 #endif
