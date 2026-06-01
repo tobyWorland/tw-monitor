ff121e5b9a24fcd8290e2f4314ee673c81888ad3
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-01 16:54:12 +0100

    Fix incorrect BFAR address

--- monitor_src/scb.c
+++ monitor_src/scb.c
@@ -9,7 +9,7 @@
 volatile uint32_t *scb_cfsr = (void*)0xE000ED28; // Configurable Fault Status Register
 volatile uint32_t *scb_hfsr = (void*)0xE000ED2C; // Hard Fault Status Register
 volatile uint32_t *scb_mmar = (void*)0xE000ED34; // Memory Management fault Address Register
-volatile uint32_t *scb_bfar = (void*)0xE000ED28; // Bus Fault Address Register
+volatile uint32_t *scb_bfar = (void*)0xE000ED38; // Bus Fault Address Register
 
 #define HFSR_FORCED      BIT(30)
 #define HFSR_VECTTBL     BIT(1)
