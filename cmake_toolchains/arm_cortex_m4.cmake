c20d727e8754a4a3227a267e60d1a62ca85e9163
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-02 18:07:06 +0100

    If a C++ cross compiler is found CMake will try to test it
    This caused some issues with the Jenkins pipeline

--- cmake_toolchains/arm_cortex_m4.cmake
+++ cmake_toolchains/arm_cortex_m4.cmake
@@ -4,3 +4,6 @@ set(CMAKE_ASM_COMPILER  "arm-none-eabi-as")
 set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffreestanding -g -Og")
 set(CMAKE_ASM_FLAGS "-g")
 set(CMAKE_C_LINK_EXECUTABLE "arm-none-eabi-ld <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
+
+set(CMAKE_CXX_COMPILER "/bin/true")
+set(CMAKE_CXX_LINK_EXECUTABLE "/bin/true")
