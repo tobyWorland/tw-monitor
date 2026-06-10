2c7f9cfbdb35672d5d9b2f9b49390570d95f594b
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-10 23:10:34 +0100

    Enable warnings (-Wall & -Wextra)

--- cmake_toolchains/arm_cortex_m4.cmake
+++ cmake_toolchains/arm_cortex_m4.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_C_COMPILER  "arm-none-eabi-gcc")
 set(CMAKE_ASM_COMPILER  "arm-none-eabi-as")
-set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffreestanding -g -Og")
+set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffreestanding -g -Og -Wall -Wextra")
 set(CMAKE_ASM_FLAGS "-g")
 set(CMAKE_C_LINK_EXECUTABLE "arm-none-eabi-ld <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 
