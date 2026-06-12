5dcfba96287417ada6450f66764ab932637a55af
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-12 22:25:37 +0100

    Add fake_memory to make HOST builds compile

---
+++ demos/fake_memory.c
@@ -0,0 +1,12 @@
+#include "../monitor/memory.h"
+
+#include <stddef.h>
+
+struct memory_entry *memory_rlookup_label(void *ptr) {
+    return NULL;
+}
+
+void memory_print_name_from_label(struct memory_entry *memory_entry) {
+    (void)memory_entry;
+    // Do nothing
+}
