7470a736939fac814bbf6e66e50ce5cbff9b8f89
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-18 15:24:23 +0100

    Optimise for size

--- cmake_toolchains/arm_cortex_m4.cmake
+++ cmake_toolchains/arm_cortex_m4.cmake
@@ -1,9 +1,13 @@
 set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_C_COMPILER  "arm-none-eabi-gcc")
 set(CMAKE_ASM_COMPILER  "arm-none-eabi-as")
-set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffreestanding -g -Og -Wall -Wextra")
+set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -ffreestanding -g -Wall -Wextra")
 set(CMAKE_ASM_FLAGS "-g")
 set(CMAKE_C_LINK_EXECUTABLE "arm-none-eabi-ld <LINK_FLAGS> <OBJECTS> -o <TARGET> <LINK_LIBRARIES>")
 
 set(CMAKE_CXX_COMPILER "/bin/true")
 set(CMAKE_CXX_LINK_EXECUTABLE "/bin/true")
+
+set(CMAKE_C_FLAGS_DEBUG "-Og")
+set(CMAKE_C_FLAGS_MINSIZEREL "-Os -fno-strict-aliasing -Wno-array-bounds")
+set(CMAKE_ASM_FLAGS_MINSIZEREL "") # CMake tries to give assembler -Os flag
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,11 @@
 
 cd `dirname $0`/..
 
+ARM_TOOLCHAIN="-DCMAKE_TOOLCHAIN_FILE=cmake_toolchains/arm_cortex_m4.cmake"
+
 mkdir -p builds
 cmake -DCMAKE_BUILD_TYPE=Debug -B builds/host && cmake --build builds/host
-cmake -DSRAM_ONLY=OFF -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cmake_toolchains/arm_cortex_m4.cmake -B builds/target_flash && cmake --build builds/target_flash
-cmake -DSRAM_ONLY=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=cmake_toolchains/arm_cortex_m4.cmake -B builds/target_sram && cmake --build builds/target_sram
+cmake $ARM_TOOLCHAIN -DSRAM_ONLY=OFF -DCMAKE_BUILD_TYPE=MinSizeRel -B builds/target_flash  && cmake --build builds/target_flash
+cmake $ARM_TOOLCHAIN -DSRAM_ONLY=ON  -DCMAKE_BUILD_TYPE=MinSizeRel -B builds/target_sram   && cmake --build builds/target_sram
+cmake $ARM_TOOLCHAIN -DSRAM_ONLY=OFF -DCMAKE_BUILD_TYPE=Debug -B builds/debug_target_flash && cmake --build builds/debug_target_flash
+cmake $ARM_TOOLCHAIN -DSRAM_ONLY=ON  -DCMAKE_BUILD_TYPE=Debug -B builds/debug_target_sram  && cmake --build builds/debug_target_sram
