783a334b4735da4d49f8ecbf929066047b0f3d32
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-28 00:16:13 +0100

    Add nvic_disable_irq() and nvic_clear_irq()

--- monitor/arm/nvic.c
+++ monitor/arm/nvic.c
@@ -11,6 +11,10 @@
 #define NVIC_IRQ_PER_ISER 32
 #define NVIC_ISERX_COUNT 8
 
+#define NVIC_ICPR0 (void *)0xE000E280
+#define NVIC_IRQ_PER_ICPR 32
+#define NVIC_ICPRX_COUNT 8
+
 void nvic_disable_all_irqs(void) {
     volatile uint32_t *iser = NVIC_ISER0;
 
@@ -19,7 +23,7 @@ void nvic_disable_all_irqs(void) {
     }
 }
 
-void nvic_enable_irq(unsigned irq) {
+void nvic_set_enable_irq(unsigned irq, bool enable) {
     assert(irq < NVIC_MAX_IRQ);
     volatile uint32_t *iser = NVIC_ISER0;
 
@@ -28,5 +32,29 @@ void nvic_enable_irq(unsigned irq) {
     iser += irq / NVIC_IRQ_PER_ISER;
     irq %= NVIC_IRQ_PER_ISER;
 
-    *iser |= (1U << irq);
+    if (enable) {
+        *iser |= (1U << irq);
+    } else {
+        *iser &= ~(1U << irq);
+    }
+}
+
+void nvic_enable_irq(unsigned irq) {
+    nvic_set_enable_irq(irq, true);
+}
+
+void nvic_disable_irq(unsigned irq) {
+    nvic_set_enable_irq(irq, false);
+}
+
+void nvic_clear_irq(unsigned irq) {
+    assert(irq < NVIC_MAX_IRQ);
+    volatile uint32_t *icpr = NVIC_ICPR0;
+
+    // Find the correct icpr register and bit value
+    // 32 per ICPR, so IRQ 33 would be ICPR1, BIT 1
+    icpr += irq / NVIC_IRQ_PER_ICPR;
+    irq %= NVIC_IRQ_PER_ICPR;
+
+    *icpr |= (1U << irq);
 }
--- monitor/arm/nvic.h
+++ monitor/arm/nvic.h
@@ -1,7 +1,12 @@
 #ifndef NVIC_H_INCLUDED
 #define NVIC_H_INCLUDED
 
+#include <stdbool.h>
+
 void nvic_disable_all_irqs(void);
+void nvic_set_enable_irq(unsigned irq, bool enable);
 void nvic_enable_irq(unsigned irq);
+void nvic_disable_irq(unsigned irq);
+void nvic_clear_irq(unsigned irq);
 
 #endif
