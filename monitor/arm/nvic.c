b177a9e86f56408317fe1cb27914a068ce271586
Toby Worland <47527939+tobyWorland@users.noreply.github.com>
2026-06-27 23:07:56 +0100

    Support all IRQs in NVIC functions

--- monitor/arm/nvic.c
+++ monitor/arm/nvic.c
@@ -4,14 +4,29 @@
 
 #include <stdint.h>
 
+// 240 is the max according the Cortex-M4 Devices Generic User Guide
+#define NVIC_MAX_IRQ 240
+
+#define NVIC_ISER0 (void *)0xE000E100
+#define NVIC_IRQ_PER_ISER 32
+#define NVIC_ISERX_COUNT 8
+
 void nvic_disable_all_irqs(void) {
-    // TODO: Support all IRQs
-    volatile uint32_t *NVIC_ISER0 = (void*)0xE000E100;
-    *NVIC_ISER0 = 0;
+    volatile uint32_t *iser = NVIC_ISER0;
+
+    for (int i = 0; i < NVIC_ISERX_COUNT; i++, iser++) {
+        *iser = 0;
+    }
 }
 
 void nvic_enable_irq(unsigned irq) {
-    assert(irq < 32); // TODO: Support all IRQs
-    volatile uint32_t *NVIC_ISER0 = (void*)0xE000E100;
-    *NVIC_ISER0 |= (1U << irq);
+    assert(irq < NVIC_MAX_IRQ);
+    volatile uint32_t *iser = NVIC_ISER0;
+
+    // Find the correct iser register and bit value
+    // 32 per ISER, so IRQ 33 would be ISER1, BIT 1
+    iser += irq / NVIC_IRQ_PER_ISER;
+    irq %= NVIC_IRQ_PER_ISER;
+
+    *iser |= (1U << irq);
 }
